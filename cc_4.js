152f6d3602827ca156d19ae9a7bd6b0a776a1165
Kiefer Bairstow <bairstow@usf.edu>
2025-09-18 12:07:56 -0400

    Created products

---
+++ cc_4.js
@@ -0,0 +1,12 @@
+// Coding Challenge 04
+
+// Creating Products
+let products = [    
+    {name: "Laptop", category: "electronics", price: 275, inventory: 36},
+    {name: "T-Shirts", category: "apparel", price: 15.99, inventory: 47},
+    {name: "Charger", category: "electronics", price: 5.99, inventory: 78},
+    {name: "Plates", category: "home", price: 10, inventory: 50},
+    {name: "Shoes", category: "apparel", price: 80.99, inventory: 12},
+];
+
+// for of loop
