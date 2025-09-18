6e07405f53218814f0714841d28a5354bb9f13fb
Kiefer Bairstow <bairstow@usf.edu>
2025-09-18 16:15:40 -0400

    corrected spelling & added new customer carmine

--- cc_4.js
+++ cc_4.js
@@ -4,7 +4,7 @@
 let products = [    
     {name: "Laptop", category: "electronics", price: 275, inventory: 36},
     {name: "T-Shirts", category: "apparel", price: 15.99, inventory: 47},
-    {name: "Charger", category: "electronics", price: 5.99, inventory: 78},
+    {name: "Watch", category: "accessory", price: 50, inventory: 78},
     {name: "Plates", category: "home", price: 10, inventory: 50},
     {name: "Shoes", category: "apparel", price: 80.99, inventory: 12},
 ];
@@ -16,15 +16,15 @@ for (let product of products) {
  // Create switch    
     switch (product.category) {
          case "electronics":
-             discount = .15
+             discount = .20
          break;
             
         case "apparel":
-            discount = .10
+            discount = .15
         break;
 
         case "home":
-            discount = .05
+            discount = .10
         break;
 
         default: 
@@ -40,19 +40,20 @@ for (let product of products) {
 let customerType = "standard";
 let customerDiscount = 0; //discount variable
 
-if (customerType === "student") {
-    customerDiscount = 0.10; // 10% Off for students
-} else if (customerType === "senior") {
-    customerDiscount = .05; // 5% Off for seniors
+if (customerType === "senior") {
+    customerDiscount = .07; // 7% Off for seniors
+} else if (customerType === "student") {
+    customerDiscount = .05; // 5% Off for students
 } else {
     customerDiscount = 0; // regular customers
- }
+ };
 
 // Creating Customers
  let customers = [
-    {name: "Bob" , type: "student", cart: [products[0], products[2]]}, // Laptop + Charger
-    {name: "Jimmy" , type: "senior", cart: [products[1], products[3]]}, // T-Shirts + Plates
-    {name: "Sandra" , type: "standard", cart: [products[4]]}, // Shoes
+    {name: "Bob" , type: "student", cart: [products[0]]}, // Laptop 
+    {name: "Jimmy" , type: "senior", cart: [products[1]]}, // T-Shirts 
+    {name: "Carmine" , type: "senior", cart: [products[3]]}, // Plates
+    {name: "Sandra" , type: "standard", cart: [products[2]]}, // Watch
  ];
 
 // Simulate Checkout Process
@@ -63,17 +64,17 @@ for (let item of customer.cart) {
     subtotal += item.promoPrice * 1; // Use Promo Price
     item.inventory -=1;   //Reduce Inventory 
 }
-    customerType = customer.type;
-    if (customerType === "student") {
-    customerDiscount = 0.05; 
-  } else if (customerType === "senior") {
-    customerDiscount = 0.07; 
-  } else {
+customerType = customer.type;
+     if (customerType === "senior") {
+    customerDiscount = .07; 
+} else if (customerType === "student") {
+     customerDiscount = .05; 
+} else {
     customerDiscount = 0;
   }
 
   // Apply Customer Discount
-let finalTotal = subtotal * (1-customerDiscount);
+let finalTotal = subtotal * (1-customerDiscount); // Makes final total
   
   console.log(`Customer ${i + 1}: ${customer.name} (${customer.type})`);
   console.log(`Total cost: $${finalTotal.toFixed(2)}`);
